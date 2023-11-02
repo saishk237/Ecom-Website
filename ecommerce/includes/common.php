d5ddb9f644b627b8e92bb7b7da7cc7521a58e795
Saish Kothawade <saish.kothawade20@pccoepune.org>
2023-11-02 12:35:00 +0530

    Update common.php

--- ecommerce/includes/common.php
+++ ecommerce/includes/common.php
@@ -1,6 +1,6 @@
 <!--establish the connection to database, and start the session-->
 <?php
-$con = mysqli_connect("localhost", "root", "", "store")or die($mysqli_error($con));
+$con = mysqli_connect("65.2.132.9", "root", "", "store")or die($mysqli_error($con));
 if(session_status() == PHP_SESSION_NONE){
     session_start();
 }
